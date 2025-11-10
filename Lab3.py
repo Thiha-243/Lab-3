d7dfb8fd1d31cdd0e4ba7838dc88c677367b198e
Thiha <thhtut.24@ichat.sp.edu.sg>
2025-11-10 09:48:00 +0800

    Updated with requirements

--- Lab3.py
+++ Lab3.py
@@ -6,35 +6,43 @@ SORT_DESCENDING = 1
 
 def bubble_sort(arr, sorting_order):
 
+    if not all(isinstance(x, int) for x in arr):
+        return 2
+
     # Copy input list to results list
     arr_result = arr.copy()
 
     # Get number of elements in the list
     n = len(arr_result)
 
-    if n < 10:
+    if n == 0:
+        return 0
+
+    if n >= 10:
+        return 1
+    
+    
+
+    
         # Traverse through all array elements
-        for i in range(n - 1):
-            # range(n) also work but outer loop will
-            # repeat one time more than needed.
+    for i in range(n - 1):
+        # range(n) also work but outer loop will
+        # repeat one time more than needed.
 
-            # Last i elements are already in place
-            for j in range(0, n - i - 1):
+        # Last i elements are already in place
+        for j in range(0, n - i - 1):
 
-                if sorting_order == SORT_ASCENDING:
-                    if arr_result[j] > arr_result[j + 1]:
-                        arr_result[j], arr_result[j + 1] = arr_result[j + 1], arr_result[j]
+            if sorting_order == SORT_ASCENDING:
+                if arr_result[j] > arr_result[j + 1]:
+                    arr_result[j], arr_result[j + 1] = arr_result[j + 1], arr_result[j]
 
 
-                elif sorting_order == SORT_DESCENDING:
-                    if arr_result[j] < arr_result[j + 1]:
-                        arr_result[j], arr_result[j + 1] = arr_result[j + 1], arr_result[j]
+            elif sorting_order == SORT_DESCENDING:
+                if arr_result[j] < arr_result[j + 1]:
+                    arr_result[j], arr_result[j + 1] = arr_result[j + 1], arr_result[j]
 
-                else:
-                    # Return an empty array
-                    arr_result = []
-    else:
-        arr_result = -1
+           
+    
 
     return arr_result
 
@@ -51,6 +59,22 @@ def main():
     print("Sorted array in descending order: ")
     result = bubble_sort(arr, SORT_DESCENDING)
     print(result)
+
+    print("Sorted array that are >= 10")
+    arr1 = list(range(10))
+    result = bubble_sort(arr1,SORT_ASCENDING)
+    print(">=10 numbers : "+ str(result))
+
+    print("Sorted array of 0 numbers")
+    arr2 = []
+    result = bubble_sort(arr2,SORT_ASCENDING)
+    print("No numbers : "+ str(result))
+
+    print("Sorted array of non-numeric numbers")
+    arr3 = ["a" , "b", "c"]
+    result = bubble_sort(arr3,SORT_ASCENDING)
+    print("Non-numeric numbers : "+ str(result))
+
 
 if __name__ == "__main__":
     main()
